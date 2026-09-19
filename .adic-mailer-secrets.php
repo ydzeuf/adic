be4115d4bd44b57350f804310ea4c4683661b927
Youssef OMRI <ydzeuf@gmail.com>
2026-09-19 21:06:45 +0300

    [SSL]

---
+++ .adic-mailer-secrets.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'brevo_api_key'    => 'YOUR_NEW_BREVO_API_KEY',
+    'sender_email'     => 'YOUR_VERIFIED_BREVO_SENDER_EMAIL',
+    'instructor_email' => 'sys_wp@marakn.com'
+];
