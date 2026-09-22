f8daaeb7246be21ef3130fbd9e12564d4fb4517c
Youssef OMRI <ydzeuf@gmail.com>
2026-09-22 19:31:05 +0300

    [Resend]

--- .adic-mailer-secrets.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'resend_api_key'    => 'YOUR_NEW_BREVO_API_KEY',
-    'sender_email'     => 'YOUR_VERIFIED_BREVO_SENDER_EMAIL',
-    'instructor_email' => 'sys_wp@marakn.com'
-];
